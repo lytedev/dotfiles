f8a69c995e10ceacef1ce23dfbafa075bc2f9336
user wheel <daniel@localhost.localdomain>
2014-12-10 14:46:18 -0600

    More fixes and changes

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,7 +6,9 @@
 pacman-key -f 962DDE58
 pacman-key --lsign 962DDE58
 
-sudo echo "[infinality-bundle]\nServer = http://bohoomil.com/repo/\$arch\n\n[infinality-bundle-fonts]\nServer = http://bohoomil.com/repo/fonts\n" >> /etc/pacman.conf
+sudo echo -e "[infinality-bundle]\nServer = http://bohoomil.com/repo/\$arch\n\n[infinality-bundle-fonts]\nServer = http://bohoomil.com/repo/fonts\n" >> /etc/pacman.conf
+
+pacman -Syyu --noconfirm
 
 pacman -S --noconfirm git zsh python luakit xorg-server xorg-server-utils xorg-xinit xorg-xrandr vim curl openssh openssl sudo python2 tmux terminus-font ttf-inconsolata unzip libxcb xcb-util xcb-util-keysyms xcb-util-wm gcc make rxvt-unicode yajl expac infinality-bundle
 
