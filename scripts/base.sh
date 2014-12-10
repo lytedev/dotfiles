e37faebba3a7a534c9df94b1f5da7daa42af6046
user wheel <daniel@localhost.localdomain>
2014-12-10 13:58:48 -0600

    More script fixes

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,5 +3,5 @@
 # This script is for setting up a base arch machine
 # It assumes a "base base-devel" pacstrap. 
 
-pacman -S --noconfirm git zsh python luakit xorg-server xorg-server-utils xorg-xinit xorg-xrandr vim curl openssh openssl sudo python2 tmux terminus-font ttf-inconsolata unzip libxcb xcb-util xcb-util-keysyms xcb-util-wm gcc make rxvt-unicode yajl
+pacman -S --noconfirm git zsh python luakit xorg-server xorg-server-utils xorg-xinit xorg-xrandr vim curl openssh openssl sudo python2 tmux terminus-font ttf-inconsolata unzip libxcb xcb-util xcb-util-keysyms xcb-util-wm gcc make rxvt-unicode yajl expac
 
