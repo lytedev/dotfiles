c28b20c5aa71082b0f792577549a100a7c59976f
user wheel <daniel@localhost.localdomain>
2014-12-10 13:12:33 -0600

    Added xorg-server-utils

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,7 +3,7 @@
 # This script is for setting up a base arch machine
 # It assumes a "base base-devel" pacstrap. 
 
-pacman -S --noconfirm git zsh python luakit xorg-server xorg-xinit xorg-xrandr vim curl openssh openssl sudo python2 tmux ttf-opensans terminus-font ttf-inconsolata unzip libxcb xcb-util xcb-util-keysyms xcb-util-wm gcc make rxvt-unicode
+pacman -S --noconfirm git zsh python luakit xorg-server xorg-server-utils xorg-xinit xorg-xrandr vim curl openssh openssl sudo python2 tmux ttf-opensans terminus-font ttf-inconsolata unzip libxcb xcb-util xcb-util-keysyms xcb-util-wm gcc make rxvt-unicode
 
 xset +fp /usr/share/fonts/TTF
 xset +fp /usr/share/fonts/OTF
