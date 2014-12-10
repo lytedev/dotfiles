f4f44e79af5d028c2e33852e5489e696e9c3c20b
user wheel <daniel@localhost.localdomain>
2014-12-10 14:07:52 -0600

    Fixed moar stuff

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,5 +3,9 @@
 # This script is for setting up a base arch machine
 # It assumes a "base base-devel" pacstrap. 
 
-pacman -S --noconfirm git zsh python luakit xorg-server xorg-server-utils xorg-xinit xorg-xrandr vim curl openssh openssl sudo python2 tmux terminus-font ttf-inconsolata unzip libxcb xcb-util xcb-util-keysyms xcb-util-wm gcc make rxvt-unicode yajl expac
+pacman-key -r 962DDE58
+
+sudo echo "[infinality-bundle]\nServer = http://bohoomil.com/repo/\$arch\n\n[infinality-bundle-fonts]\nServer = http://bohoomil.com/repo/fonts\n" >> /etc/pacman.conf
+
+pacman -S --noconfirm git zsh python luakit xorg-server xorg-server-utils xorg-xinit xorg-xrandr vim curl openssh openssl sudo python2 tmux terminus-font ttf-inconsolata unzip libxcb xcb-util xcb-util-keysyms xcb-util-wm gcc make rxvt-unicode yajl expac infinality-bundle
 
