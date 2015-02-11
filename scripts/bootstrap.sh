ae41879550cc6f7fdd8102fc443bfb25a3b5d6d6
Daniel Flanagan <daniel@lytedev.io>
2015-02-11 11:39:08 -0500

    Some changes

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,17 +3,9 @@
 # Get source directory
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )/.." && pwd)
 
-# Create necessary config directories
-mkdir -p $HOME/.config
-mkdir -p $HOME/.config/bspwm
-mkdir -p $HOME/.config/sxhkd
-
 # Link files
 ln -s $DIR/luakit $HOME/.config/luakit
 ln -s $DIR/shell/zshrc $HOME/.zshrc
 ln -s $DIR/shell/bashrc $HOME/.bashrc
 ln -s $DIR/vim/vimrc $HOME/.vimrc
-ln -s $DIR/xorg/xinitrc $HOME/.xinitrc
-ln -s $DIR/xorg/Xresources $HOME/.Xresources
-ln -s $DIR/wm/bspwmrc $HOME/.config/bspwm/bspwmrc
-ln -s $DIR/wm/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+
--- scripts/user.sh
+++ scripts/user.sh
@@ -3,40 +3,6 @@
 # Create user directories
 mkdir $HOME/code
 mkdir $HOME/code/repos
-mkdir $HOME/code/aur
 mkdir $HOME/downloads
 
-# Get pacaur for managing AUR packages
-cd $HOME/code/aur
-
-curl -O https://aur.archlinux.org/packages/co/cower/cower.tar.gz
-curl -O https://aur.archlinux.org/packages/pa/pacaur/pacaur.tar.gz
-
-tar -xzf cower.tar.gz
-tar -xzf pacaur.tar.gz
-
-cd cower
-makepkg -i --noconfirm
-
-cd ../pacaur
-makepkg -i --noconfirm 
-
-cd ..
-rm -f *.tar.gz
-
-pacaur -S --noconfirm ttf-opensans
-
-# Get our window manager code
-cd $HOME/code/repos
-
-git clone https://github.com/baskerville/bspwm.git
-git clone https://github.com/baskerville/sxhkd.git
-
-cd bspwm
-make
-sudo make install
-
-cd ../sxhkd
-make
-sudo make install
 
