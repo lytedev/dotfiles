bfac37c9d3de12d77ec3e223e33c323ebef16510
Daniel Flanagan <dflanagan@emfluence.com>
2015-02-11 14:40:24 -0600

    More OSX work

--- scripts/osx-base.sh
+++ scripts/osx-base.sh
@@ -8,4 +8,5 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt --with-default-names
 
 # Tell .bash_profile about .bashrc and prepend the gnubin to our PATH
-echo -e '\nsource $HOME/.bashrc\nPATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"\nexport PATH' > $HOME/.bash_profile
+echo -e '#!/usr/bin/env bash\n\nsource $HOME/.bashrc\nPATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"\nexport PATH' > $HOME/.bash_profile
+
