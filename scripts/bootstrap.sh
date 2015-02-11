fc7619673a80ad8ab1274ae6ef71e6d0da78b1bd
Daniel Flanagan <dflanagan@emfluence.com>
2015-02-11 14:04:48 -0600

    Work on adaptiation for osx

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -4,6 +4,11 @@
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )/.." && pwd)
 
 # Link files
+rm -r $HOME/.config/luakit
+rm -r $HOME/.zshrc
+rm -r $HOME/.bashrc
+rm -r $HOME/.vimrc
+rm -r $HOME/.tmux.conf
 ln -s $DIR/luakit $HOME/.config/luakit
 ln -s $DIR/shell/zshrc $HOME/.zshrc
 ln -s $DIR/shell/bashrc $HOME/.bashrc
