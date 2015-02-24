044455a502c5fc02460fc42cb1b912e9483c0b6e
Daniel Flanagan <dflanagan@emfluence.com>
2015-02-24 12:47:07 -0600

    Added neovim config

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -11,10 +11,12 @@ rm -r $HOME/.vimrc
 rm -r $HOME/.tmux.conf
 rm -r $HOME/.irssi
 rm -r $HOME/.muttrc
+rm -r $HOME/.nvimrc
 ln -s $DIR/apps/luakit $HOME/.config/luakit
 ln -s $DIR/shell/zshrc $HOME/.zshrc
 ln -s $DIR/shell/bashrc $HOME/.bashrc
 ln -s $DIR/apps/vim/vimrc $HOME/.vimrc
+ln -s $DIR/apps/nvim/nvimrc $HOME/.nvimrc
 ln -s $DIR/shell/tmux.conf $HOME/.tmux.conf
 ln -s $DIR/apps/irssi $HOME/.irssi
 ln -s $DIR/apps/mutt/muttrc $HOME/.muttrc
