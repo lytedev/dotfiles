2c7abd5c0c12a6f3ef813503064563a5f7de6f2e
Daniel Flanagan <dflanagan@emfluence.com>
2015-03-09 13:04:46 -0500

    more vim stuff

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+# You'll want to have neovim (nvim) installed first!
+
 # Get source directory
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )/.." && pwd)
+
+# We need to do some super quick setup for neovim plugins
+echo "Setting up neovim plugins"
+mkdir -p $HOME/.nvim
+if [ -d "$HOME/.nvim/bundle/Vundle.vim" ]; then
+    echo "Vundle already setup"
+else
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.nvim/bundle/Vundle.vim
+fi
+
+echo "Installing vim plugins"
+nvim +PluginInstall +qall
+
+echo "Creating symlinks"
 
 # Link files
 rm -r $HOME/.config/luakit
@@ -15,7 +31,7 @@ rm -r $HOME/.nvimrc
 ln -s $DIR/apps/luakit $HOME/.config/luakit
 ln -s $DIR/shell/zshrc $HOME/.zshrc
 ln -s $DIR/shell/bashrc $HOME/.bashrc
-ln -s $DIR/apps/vim/vimrc $HOME/.vimrc
+ln -s $DIR/apps/nvim/nvimrc $HOME/.vimrc
 ln -s $DIR/apps/nvim/nvimrc $HOME/.nvimrc
 ln -s $DIR/shell/tmux.conf $HOME/.tmux.conf
 ln -s $DIR/apps/irssi $HOME/.irssi
