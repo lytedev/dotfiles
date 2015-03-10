f482dede033ec2761b7e15b5f45a5d9e260f9810
Daniel Flanagan <dflanagan@emfluence.com>
2015-03-10 12:48:08 -0500

    Added script for setting up keepass on OSX

---
+++ scripts/platform_setup/extra/osx_keepass_setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+brew install wget
+brew install cpanminus
+sudo cpanm Crypt::Rijndael
+sudo cpanm Sort::Naturally
+sudo cpanm Term::ShellUI
+sudo cpanm File::KeePass
+sudo cpanm Term::ReadLine::Gnu
+sudo cpanm Term::ReadLine::Perl
+wget 'http://downloads.sourceforge.net/project/kpcli/kpcli-2.8.pl?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fkpcli%2F&ts=1426009460&use_mirror=iweb'
+mv kpcli-*.pl* $HOME/Applications/kpcli.pl
+sudo chmod +x $HOME/Applications/kpcli.pl
+
