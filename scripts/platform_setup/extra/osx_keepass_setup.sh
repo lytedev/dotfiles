4682f5e846e6f7a9d2b599b3a2b5adfdb8c66dd1
Daniel Flanagan <dflanagan@emfluence.com>
2015-03-10 13:21:09 -0500

    added necessary perl modules for clipboard copying for osx keepass setup script

--- scripts/platform_setup/extra/osx_keepass_setup.sh
+++ scripts/platform_setup/extra/osx_keepass_setup.sh
@@ -8,6 +8,8 @@ sudo cpanm Term::ShellUI
 sudo cpanm File::KeePass
 sudo cpanm Term::ReadLine::Gnu
 sudo cpanm Term::ReadLine::Perl
+sudo cpanm Clipboard --force
+sudo cpanm Capture::Tiny
 wget 'http://downloads.sourceforge.net/project/kpcli/kpcli-2.8.pl?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fkpcli%2F&ts=1426009460&use_mirror=iweb'
 mv kpcli-*.pl* $HOME/Applications/kpcli.pl
 sudo chmod +x $HOME/Applications/kpcli.pl
