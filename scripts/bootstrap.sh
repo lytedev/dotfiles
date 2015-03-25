c897cea259d8cfb407a8d0560eb6c26f24ad2c8e
Daniel Flanagan <daniel@lytedev.io>
2015-03-25 15:45:33 -0500

    bugfixes for bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -20,6 +20,46 @@ else
         exit 1
     fi
 fi
+echo "Creating symlinks"
+
+# Link files
+rm -rf $HOME/.config/luakit
+rm -rf $HOME/.bashrc
+rm -rf $HOME/.vimrc
+rm -rf $HOME/.tmux.conf
+# rm -rf $HOME/.irssi
+rm -rf $HOME/.newsbeuter
+rm -rf $HOME/.weechat
+rm -rf $HOME/.muttrc
+rm -rf $HOME/.nvimrc
+rm -rf $HOME/.config/bar
+rm -rf $HOME/.gitconfig
+rm -rf $HOME/.nvim
+rm -rf $HOME/.vim
+ln -s $DIR/apps/luakit $HOME/.config/luakit
+ln -s $DIR/shell/bashrc $HOME/.bashrc
+ln -s $DIR/apps/nvim/nvimrc $HOME/.vimrc
+ln -s $DIR/apps/nvim/nvimrc $HOME/.nvimrc
+ln -s $DIR/shell/tmux.conf $HOME/.tmux.conf
+# ln -s $DIR/apps/irssi $HOME/.irssi
+ln -s $DIR/apps/newsbeuter $HOME/.newsbeuter
+ln -s $DIR/apps/weechat $HOME/.weechat
+ln -s $DIR/apps/mutt/muttrc $HOME/.muttrc
+ln -s $DIR/wm/bar $HOME/.config/bar
+ln -s $DIR/shell/gitconfig $HOME/.gitconfig
+ln -s $DIR/apps/nvim/nvim $HOME/.vim
+ln -s $DIR/apps/nvim/nvim $HOME/.nvim
+
+# Sublime config location varies by OS
+if [ "Darwin" = "$(uname -s)" ]; then
+    # Assume OSX
+    rm -rf "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+    ln -s "$DIR/apps/sublime_text/user_package" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+else
+    # Assume Linux
+    rm -rf "$HOME/.config/sublime-text-3/Packages/User"
+    ln -s "$DIR/apps/sublime_text/user_package" "$HOME/.config/sublime-text-3/Packages/User"
+fi 
 
 # We need to do some super quick setup for neovim plugins
 echo "Setting up vim plugins"
@@ -42,43 +82,6 @@ else
     "./install.sh"
     cd "$CUR_PWD"
 fi
-
-echo "Creating symlinks"
-
-# Link files
-rm -rf $HOME/.config/luakit
-rm -rf $HOME/.bashrc
-rm -rf $HOME/.vimrc
-rm -rf $HOME/.tmux.conf
-# rm -rf $HOME/.irssi
-rm -rf $HOME/.newsbeuter
-rm -rf $HOME/.weechat
-rm -rf $HOME/.muttrc
-rm -rf $HOME/.nvimrc
-rm -rf $HOME/.config/bar
-rm -rf $HOME/.gitconfig
-ln -s $DIR/apps/luakit $HOME/.config/luakit
-ln -s $DIR/shell/bashrc $HOME/.bashrc
-ln -s $DIR/apps/nvim/nvimrc $HOME/.vimrc
-ln -s $DIR/apps/nvim/nvimrc $HOME/.nvimrc
-ln -s $DIR/shell/tmux.conf $HOME/.tmux.conf
-# ln -s $DIR/apps/irssi $HOME/.irssi
-ln -s $DIR/apps/newsbeuter $HOME/.newsbeuter
-ln -s $DIR/apps/weechat $HOME/.weechat
-ln -s $DIR/apps/mutt/muttrc $HOME/.muttrc
-ln -s $DIR/wm/bar $HOME/.config/bar
-ln -s $DIR/shell/gitconfig $HOME/.gitconfig
-
-# Sublime config location varies by OS
-if [ "Darwin" = "$(uname -s)" ]; then
-    # Assume OSX
-    rm -rf "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-    ln -s "$DIR/apps/sublime_text/user_package" "$HOME/Library/Application Support/Sublime Text 3/Packages/User"
-else
-    # Assume Linux
-    rm -rf "$HOME/.config/sublime-text-3/Packages/User"
-    ln -s "$DIR/apps/sublime_text/user_package" "$HOME/.config/sublime-text-3/Packages/User"
-fi 
 
 echo "Adding DOTFILES_PATH to .bashrc"
 sed -i "/export DOTFILES_PATH/c export DOTFILES_PATH=\"$DIR\"" "$HOME/.bashrc"
