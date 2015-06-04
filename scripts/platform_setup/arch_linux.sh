84fdf617a57f4235152c5f5e1c701e5c10052c8d
Daniel Flanagan <daniel@lytedev.io>
2015-06-04 13:41:27 -0500

    added alternate sxhkdrc and switchers

--- scripts/platform_setup/arch_linux.sh
+++ scripts/platform_setup/arch_linux.sh
@@ -52,10 +52,12 @@ rm -f "$HOME/.xinitrc"
 rm -f "$HOME/.Xresources"
 rm -f "$HOME/.config/bspwm/bspwmrc"
 rm -f "$HOME/.config/sxhkd/sxhkdrc"
+rm -f "$HOME/.config/sxhkd/altsxhkdrc"
 ln -s "$DIR/wm/xorg/xinitrc" "$HOME/.xinitrc"
 ln -s "$DIR/wm/xorg/Xresources" "$HOME/.Xresources"
 ln -s "$DIR/wm/bspwmrc" "$HOME/.config/bspwm/bspwmrc"
 ln -s "$DIR/wm/sxhkdrc" "$HOME/.config/sxhkd/sxhkdrc"
+ln -s "$DIR/wm/altsxhkdrc" "$HOME/.config/sxhkd/altsxhkdrc"
 
 # Make AUR repo folder
 mkdir -p "$HOME/code/aur"
