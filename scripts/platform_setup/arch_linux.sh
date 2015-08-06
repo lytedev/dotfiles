6567cc1dda72f83047880fe3fb6d5df3c2b4d744
Daniel Flanagan <daniel@lytedev.io>
2015-08-06 01:05:38 -0500

    added bash-completion as package

--- scripts/platform_setup/arch_linux.sh
+++ scripts/platform_setup/arch_linux.sh
@@ -39,7 +39,7 @@ sudo pacman -S --noconfirm git zsh python luakit xorg-server \
     cmake the_silver_searcher inotify-tools sysstat thunar alsa-utils feh \
     imagemagick xf86-video-intel mesa-libgl libva libva-intel-driver mpd \
     ncmpcpp nodejs npm pulseaudio pulseaudio-alsa lib32-libpulse \
-    lib32-alsa-plugins irssi xorg-xprop xorg-wininfo
+    lib32-alsa-plugins irssi xorg-xprop xorg-wininfo bash-completion
 
 # Some manual work required here
 # TODO: Flag that lets you force existing package override
