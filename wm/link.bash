66017dbe0fb385ff2d2d3603020032d2edfec5b1
Daniel Flanagan <daniel@lytedev.io>
2016-01-06 12:24:50 -0600

    more refactor

--- wm/link.bash
+++ wm/link.bash
@@ -5,6 +5,6 @@ source "$DIR/variables.bash"
 
 rm -f "$HOME/.config/bspwm/bspwmrc"
 rm -f "$HOME/.config/sxhkd/sxhkdrc"
-ln -s "$DOTFILES_PATH/de/bspwmrc" "$HOME/.config/bspwm/bspwmrc"
-ln -s "$DOTFILES_PATH/de/sxhkdrc" "$HOME/.config/sxhkd/sxhkdrc"
+ln -s "$DOTFILES_PATH/wm/bspwmrc" "$HOME/.config/bspwm/bspwmrc"
+ln -s "$DOTFILES_PATH/wm/sxhkdrc" "$HOME/.config/sxhkd/sxhkdrc"
 
