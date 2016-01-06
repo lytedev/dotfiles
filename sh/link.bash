7ed193d7d7681e1fa2ccac4613507e52e7f6ac11
Daniel Flanagan <daniel@lytedev.io>
2016-01-06 12:34:29 -0600

    added stuff...

--- sh/link.bash
+++ sh/link.bash
@@ -8,7 +8,7 @@ rm "$HOME/.bash_profile"
 rm "$HOME/.tmux.conf"
 ln -s "$DOTFILES_PATH/sh/bashrc" "$HOME/.bashrc"
 ln -s "$DOTFILES_PATH/sh/bash_profile" "$HOME/.bash_profile"
-ln -s "$DOTFILES_PATH/sh/.tmux.conf" "$HOME/.tmux.conf"
+ln -s "$DOTFILES_PATH/sh/tmux.conf" "$HOME/.tmux.conf"
 
 sed -i "/export DOTFILES_PATH/c export DOTFILES_PATH=\"$DIR\"" "$HOME/.bashrc"
 
