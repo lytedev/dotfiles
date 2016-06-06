7fcd180fda5f7e4f6a66f142e1273f6921781cb8
Daniel Flanagan <daniel@lytedev.io>
2016-06-06 15:22:00 -0500

    install wm stuff via pacaur

--- arch_linux_init/wm.bash
+++ arch_linux_init/wm.bash
@@ -4,25 +4,5 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )/.." && pwd)
 source "$DIR/variables.bash"
 
 sudo pacman -S xcb-util xcb-util-keysyms xcb-util-wm xorg-xrandr xdotool feh --noconfirm
-
-mkdir -p "$REPOSITORY_PATH"
-
-git clone https://github.com/baskerville/bspwm.git "$REPOSITORY_PATH/bspwm"
-git clone https://github.com/baskerville/xdo.git "$REPOSITORY_PATH/xdo"
-git clone https://github.com/baskerville/sxhkd.git "$REPOSITORY_PATH/sxhkd"
-
-cd "$REPOSITORY_PATH/xdo"
-make
-sudo make install
-cd -
-
-cd "$REPOSITORY_PATH/bspwm"
-make
-sudo make install
-cd -
-
-cd "$REPOSITORY_PATH/sxhkd"
-make
-sudo make install
-cd -
+pacaur -S bspwm-git sxhkd-git xdo-git
 
