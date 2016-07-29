92fef3b39126c21fb9e2b150d1d6d4319b086f42
Daniel Flanagan <daniel@lytedev.io>
2016-07-29 11:23:51 -0500

    more moving stuff around

--- variables.bash
+++ variables.bash
@@ -7,14 +7,15 @@ export DOTFILES_PATH="$HOME/.dotfiles"
 export REPOSITORY_PATH="$HOME/Documents/open-source"
 export BORDER_WIDTH=1
 export WINDOW_GAP=5
-export BAR_PID_FILE="$DOTFILES_PATH/wm_bar.pid"
+export BAR_PATH="$DOTFILES_PATH/env/wm/extras/bar"
+export BAR_PID_FILE="$BAR_PATH/wm_bar.pid"
 
 source "$DOTFILES_PATH/scripts/get_x_fonts.sh"
 source "$DOTFILES_PATH/scripts/get_color.sh"
 
 export BAR_ON_XINIT=0
-export BAR_FIFO="$DOTFILES_PATH/wm_bar_fifo.lock"
-export BAR_LOG="$DOTFILES_PATH/wm_bar.log"
+export BAR_FIFO="$BAR_PATH/wm_bar_fifo.lock"
+export BAR_LOG="$BAR_PATH/wm_bar.log"
 export BAR_TOP=0
 export BAR_HEIGHT=20
 export BAR_BORDER_WIDTH=1
@@ -39,7 +40,3 @@ export WINDOW_GAP=$(expr $WINDOW_GAP - $BORDER_WIDTH)
 if [ -a "$HOME/.bashrc_env" ]; then
   . "$HOME/.bashrc_env"
 fi
-
-# various paths that are used in the dotfiles
-BAR_PATH="$DOTFILES_PATH/env/wm/extras/bar"
-
