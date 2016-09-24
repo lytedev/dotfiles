2a3f25e9dc770081450a2ad31fa91dc7765822f9
Daniel Flanagan <daniel@lytedev.io>
2016-09-24 05:36:01 -0500

    ncmpcpp symlink

--- music/ncmpcpp/link.bash
+++ music/ncmpcpp/link.bash
@@ -3,6 +3,6 @@
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}" )/../.." && pwd)
 source "$DIR/variables.bash"
 
-rm -rf "$HOME/.ncmpcpp/"
-ln -s "$DOTFILES_PATH/music/ncmpcpp/" "$HOME/.ncmpcpp/"
+rm -rf "$HOME/.ncmpcpp"
+ln -s "$DOTFILES_PATH/music/ncmpcpp" "$HOME/.ncmpcpp"
 
