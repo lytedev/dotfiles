76a963b119eb9ad8ff358786a41ed2b83d59d46a
Daniel Flanagan <daniel@lytedev.io>
2017-02-08 13:08:47 -0600

    work on polybar, remove old linker scripts, fix bug in a provisioning script

--- os-specific/arch-linux/provisioning/3-extras.bash
+++ os-specific/arch-linux/provisioning/3-extras.bash
@@ -3,18 +3,18 @@
 pacaur -S \
 	gtk-theme-arc \
 	elementary-icon-theme \
-  lemonbar-xft-git \
+	lemonbar-xft-git \
 	thunar thunar-volman gvfs \
 	thunar-shares-plugin \
 	gvfs-smb \
-  terminus-font ttf-monaco gohufont artwiz-fonts phallus-fonts-git \
-  ttf-opensans ttf-dejavu \
+	terminus-font ttf-monaco gohufont artwiz-fonts phallus-fonts-git \
+	ttf-opensans ttf-dejavu \
 	avr-libc avr-gcc dfu-programmer dfu-util \
 	xorg-xbacklight \
 	gimp \
-  audacity
+	audacity \
 	reptyr \
-  cmake python2 python2-pip \
-  lightdm \
-  polybar-git \
-  --noconfirm --noedit
+	cmake python2 python2-pip \
+	lightdm \
+	polybar-git \
+	--noconfirm --noedit
