ea5debe38cf18bd4ad1b02e756e6ae6acc9a214b
Daniel Flanagan <daniel@lytedev.io>
2019-12-16 17:21:44 -0600

    WIP chat system

--- bin/lib/arch-linux/provisioning/4-comms.bash
+++ bin/lib/arch-linux/provisioning/4-comms.bash
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 pacaur --needed -S --noconfirm --noedit \
-	weechat-poljar-matrix-git weechat-discord-git
+	weechat-poljar-matrix-git weechat-discord-git wee-slack
 
-ln -s /usr/lib/weechat/python/matrix/   ~/.weechat/python/
-ln -s /usr/lib/weechat/python/matrix.py ~/.weechat/python/
-
-ln -s /usr/lib/weechat/python/matrix.py ~/.weechat/python/autoload/
+wcpm="/usr/lib/weechat/python/matrix"
+hwcp="${HOME}/.weechat/python"
+ln -s "${wcpm}/" "${hwcp}/"
+ln -s "${wcpm}.py" "${hwcp}/"
+ln -s "${wcpm}.py" "${hwcp}/autoload/"
+ln -s "/usr/lib/weechat/python/wee_slack.py" "${hwcp}/"
+ln -s "/usr/lib/weechat/python/wee_slack.py" "${hwcp}/autoload/"
 
 # Scripts are in /usr/lib/weechat/python/matrix/contrib/
