eb48146d6d125db2aec9250e826af4bc555710c9
Daniel Flanagan <daniel@lytedev.io>
2020-01-28 12:09:05 -0600

    Use curses pinentry and add hidden sources for laptop

---
+++ env/laptop/config.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+# TODO: completion
+function src-hidden-dir
+  not scount $argv && echo "No directory specified" && exit 1
+  set f $ENV_PATH/.hidden/fish.d/$argv[1]/config.fish
+  echo "Checking $f..."
+  test -f $f && source $f
+end
+
+set hc $ENV_PATH/.hidden/config.fish
+test -f $hc && source $hc
