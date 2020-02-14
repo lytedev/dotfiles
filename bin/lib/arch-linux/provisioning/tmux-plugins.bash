745432b20365c283e35d8c7d877dd96a4b0b4fd7
Daniel Flanagan <daniel@lytedev.io>
2020-02-14 12:30:09 -0600

    Tmux config

--- bin/lib/arch-linux/provisioning/tmux-plugins.bash
+++ bin/lib/arch-linux/provisioning/tmux-plugins.bash
@@ -3,15 +3,15 @@
 mkdir -p "$HOME/.config/systemd/user"
 
 # tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+git clone https://github.com/tmux-plugins/tpm "$XDG_CONFIG_HOME/tmux/plugins/tpm"
 
 # load tmux config
 tmux source-file "$HOME/.tmux.conf"
 
 # clean, install, and update plugins
-bash "$HOME/.tmux/plugins/tpm/bindings/clean_plugins"
-bash "$HOME/.tmux/plugins/tpm/bindings/install_plugins"
-bash "$HOME/.tmux/plugins/tpm/bindings/update_plugins"
+bash "$XDG_CONFIG_HOME/tmux/plugins/tpm/bindings/clean_plugins"
+bash "$XDG_CONFIG_HOME/tmux/plugins/tpm/bindings/install_plugins"
+bash "$XDG_CONFIG_HOME/tmux/plugins/tpm/bindings/update_plugins"
 
 # load tmux config
 tmux source-file "$HOME/.tmux.conf"
