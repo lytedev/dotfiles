28b04c4d4161b292cd936ed247dd9070c8056d4c
Daniel Flanagan <daniel@lytedev.io>
2020-03-23 13:23:50 -0500

    Swaaaay

--- apps/elixir/iex.exs
+++ apps/elixir/iex.exs
@@ -1,11 +1,15 @@
 Application.put_env(:elixir, :ansi_enabled, true)
 
+# PROTIP: to break, `#iex:break`
+
 IEx.configure(
   colors: [enabled: true],
   default_prompt: [
     "\e[G",    # ANSI CHA, move cursor to column 1
     :magenta,
     "%prefix", # IEx prompt variable
+    "#",
+    "%counter", # IEx prompt variable
     ">",       # plain string
     :reset
   ] |> IO.ANSI.format |> IO.chardata_to_string
