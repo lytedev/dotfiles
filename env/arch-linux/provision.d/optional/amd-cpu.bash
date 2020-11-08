ed6760395150d1bdae0fc588340423a768ecfcb1
Daniel Flanagan <daniel@lyte.dev>
2020-11-07 22:52:48 -0600

    Cleanup some more provisioning scripts

---
+++ env/arch-linux/provision.d/optional/amd-cpu.bash
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+pacman -S amd-ucode
--- env/arch-linux/provision.d/optional/amd-gpu.bash
+++ env/arch-linux/provision.d/optional/amd-gpu.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# TODO: vulkan
+# TODO: vulkan?
 yay -S \
 	xf86-video-amdgpu mesa \
 	libva-mesa-driver mesa-vdpau libva-vdpau-driver libvdpau-va-gl `# Hardware Accelerated Video`
