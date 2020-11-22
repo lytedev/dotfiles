03e94092f838ee766c22649171edde75208f725d
Daniel Flanagan <daniel@lyte.dev>
2020-11-22 09:30:11 -0600

    Work on greeting

--- apps/shell/fish/config.fish
+++ apps/shell/fish/config.fish
@@ -31,6 +31,9 @@ test -f ~/.fzf/shell/key-bindings.fish && source ~/.fzf/shell/key-bindings.fish
 function fish_greeting;
 	set_color -b black brblack
 	printf "%s@%s %s\n" $USER (hostname) (date)
+	printf "%6d   processes running\n" (ps -aux | wc -l)
+	printf "%6sGB memory available\n" (free -g | grep '^Mem:' | tr -s ' ' | cut -d ' ' -f7)
+	printf "%6d   PTYs open\n" (cat /proc/sys/kernel/pty/nr)
 end
 
 # we assume the user uses "$HOME" to just store their mess of dotfiles and other
