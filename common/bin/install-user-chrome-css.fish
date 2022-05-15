41173b15aae8e1ceaa3b4978c777d6d415096132
Daniel Flanagan <daniel.flanagan@divvypay.com>
2022-05-14 21:02:19 -0500

    Fix

--- common/bin/install-user-chrome-css.fish
+++ common/bin/install-user-chrome-css.fish
@@ -2,7 +2,7 @@
 
 set profile_dirs "$HOME/.mozilla/firefox/*.dev-edition-default"
 if test (uname) = Darwin
-	set profile_dirs "$HOME/Library/Application Support/Firefox/Profiles/*.dev-edition-default"
+	set profile_dirs $HOME/Library/Application\ Support/Firefox/Profiles/*.dev-edition-default
 end
 
 for p in $profile_dirs
