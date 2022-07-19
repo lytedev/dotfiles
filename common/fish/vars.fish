8fc42a9a642f62dc455ea0b4ef012cbf1eef4e77
Daniel Flanagan <daniel@lyte.dev>
2022-07-19 11:21:25 -0500

    Edit

--- common/fish/vars.fish
+++ common/fish/vars.fish
@@ -12,11 +12,18 @@ set --export --universal BROWSER firefox-developer-edition
 set --export --universal PAGER less
 set --export --universal LESS "-r"
 
-set --export --universal MANPAGER 'env MANWIDTH="" nvim --cmd "let g:prosession_on_startup=0" +Man!'
+set --export --universal MANPAGER 'env MANWIDTH="" "$EDITOR"'
 
 if command --search --quiet nvim
+	set --export --universal MANPAGER 'env MANWIDTH="" nvim --cmd "let g:prosession_on_startup=0" +Man!'
 	set --export --universal EDITOR nvim
 	set --export --universal VISUAL nvim
+else if command --search --quiet helix
+	set --export --universal EDITOR helix
+	set --export --universal VISUAL helix
+else if command --search --quiet hx
+	set --export --universal EDITOR hx
+	set --export --universal VISUAL hx
 else if command --search --quiet vim
 	set --export --universal EDITOR vim
 	set --export --universal VISUAL vim
