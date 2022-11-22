6912c4bdd079211094b86bbb49304e344d68664f
Daniel Flanagan <daniel@lyte.dev>
2022-11-22 14:35:47 -0600

    Caddy file server

--- host/layer/public-webserver/dotfiles-setup.d.fish
+++ host/layer/public-webserver/dotfiles-setup.d.fish
@@ -1,18 +1,19 @@
 #!/usr/bin/env fish
 
 pushd (dirname (status -f))
-set dfp $argv[1]
-set homedir $argv[2]
-set xdgconfdir $argv[3]
+# set dfp $argv[1]
+# set homedir $argv[2]
+# set xdgconfdir $argv[3]
 set layerdir (pwd)
 
 function _p
-	set source $argv[1]
-	set dest $argv[2]
-	sudo rm -rf "$dest"
-	echo "Copying $source to $dest"
-	sudo cp -r "$source" "$dest"
+	echo Copying $argv[1..-2] to $argv[-1]
+	sudo cp -r $argv[1..-2] $argv[-1]
 end
 
+sudo rm -rf /etc/caddy
 _p $layerdir/caddy /etc/caddy
-_p $layerdir/webserver-root /srv/http
+
+sudo mkdir -p /srv/http
+sudo chmod 755 /srv/http
+_p $layerdir/webserver-root/* /srv/http
