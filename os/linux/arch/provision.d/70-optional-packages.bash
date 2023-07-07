546ef45d61804be405b3993eef90517d0e3560db
Daniel Flanagan <daniel@lyte.dev>
2023-07-07 12:36:08 -0500

    Bin

--- os/linux/arch/provision.d/70-optional-packages.bash
+++ os/linux/arch/provision.d/70-optional-packages.bash
@@ -15,7 +15,7 @@ yn "Do you want to install UI packages?" && "$dir/optional/ui-packages.bash"
 paru -Sy --needed --noconfirm \
 	weechat `# IRC Client` \
 	rtx-bin `# Version Manager` \
-	bluetuith `# Disk Usage Analyzer` \
+	bluetuith-bin `# Disk Usage Analyzer` \
 	aria2 `# Downloads Manager` \
 	oath-toolkit `# TOTP Utility` \
 	pass pass-otp `# Password Management` \
