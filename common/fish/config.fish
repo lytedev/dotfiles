20064853fae9cfbff4e8b317854335b3ccb30573
Daniel Flanagan <daniel@lyte.dev>
2023-07-12 11:30:38 -0500

    Push

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -8,13 +8,7 @@ for f in vars colors functions
 	source $FISH_PATH/$f.fish
 end
 
-status --is-interactive || exit
-
-for f in prompt key-bindings
-	source $FISH_PATH/$f.fish
-end
-
-if has_command rtx
+if has_command rtx && not set --query RTX_SHELL
 	rtx activate fish | source
 else if has_command brew && test -f (brew --prefix asdf)/libexec/asdf.fish
 	set --universal --export ASDF_DIR (brew --prefix asdf)
@@ -23,10 +17,6 @@ else if test -f $HOME/.asdf/asdf.fish
 	source $HOME/.asdf/asdf.fish
 else if test -f /opt/asdf-vm/asdf.fish
 	source /opt/asdf-vm/asdf.fish
-end
-
-if test -f /usr/share/password-store/extensions/otp.bash
-	source $FISH_PATH/pass-otp-completions.fish
 end
 
 if test -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
@@ -47,14 +37,25 @@ if test -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 	set --prepend --export --global fish_user_paths $HOME/.nix-profile/bin /nix/var/nix/profiles/default/bin
 end
 
+if has_command nnn
+	source $DOTFILES_PATH/common/nnn/config.fish
+end
+
+# everything after this is ONLY relevant to interactive shells
+status --is-interactive || exit
+
+for f in prompt key-bindings
+	source $FISH_PATH/$f.fish
+end
+
+if test -f /usr/share/password-store/extensions/otp.bash
+	source $FISH_PATH/pass-otp-completions.fish
+end
+
 if set -q KITTY_INSTALLATION_DIR
 	set --global KITTY_SHELL_INTEGRATION enabled
 	source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
 	set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
-end
-
-if has_command nnn
-	source $DOTFILES_PATH/common/nnn/config.fish
 end
 
 test $PWD = $HOME && begin
@@ -63,11 +64,11 @@ end
 
 # If we're running the shell interactively from inside Kitty, assume that we will be using Kitty's multiplexing features
 # Otherwise, assume we're in a context that is not capable of "native" multiplexing features and run everything inside Zellij
-if not set --query ZELLIJ && set --query FISH_START_ZELLIJ && status is-interactive
+if not set --query ZELLIJ && set --query FISH_START_ZELLIJ
 	# simulate auto attach
   zellij attach -c
 	# simulate auto kill
   kill $fish_pid
-else if not set --query TMUX && set --query FISH_START_TMUX && status is-interactive
+else if not set --query TMUX && set --query FISH_START_TMUX
 	tmux att -t default || tmux new -s default
 end
