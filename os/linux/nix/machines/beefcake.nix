3ce038ca844ff3a571874d8e34cf3aae4be0c51f
Daniel Flanagan <daniel@lyte.dev>
2023-07-28 13:07:10 -0500

    Naming things

--- os/linux/nix/machines/beefcake.nix
+++ os/linux/nix/machines/beefcake.nix
@@ -19,7 +19,7 @@
   };
 
   sops = {
-    defaultSopsFile = ../secrets/beefcake/secrets.yaml;
+    defaultSopsFile = ../secrets/beefcake/secrets.yml;
     age = {
       sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
