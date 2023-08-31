5c98767fe02cdf1742d2b3efce3c7b606b3e439d
Daniel Flanagan <daniel@lyte.dev>
2023-08-30 23:03:42 -0500

    nix the things

--- common/fish/pass-otp-completions.fish
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env fish
-complete -c pass -f -n '__fish_pass_needs_command' -a otp -d 'Command: show TOTP code'
-complete -c pass -f -n '__fish_pass_uses_command otp' -s c -l clip -d 'Put TOTP code in clipboard'
-complete -c pass -f -n '__fish_pass_uses_command otp' -a "(__fish_pass_print_entries)"
