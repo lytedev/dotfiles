a07d3189b2a3fb75385f82fc63346c2482a9e581
Daniel Flanagan <daniel@lyte.dev>
2023-08-30 23:04:44 -0500

    Inputs

--- os/linux/nix/machines/beefcake.nix
+++ os/linux/nix/machines/beefcake.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running 'nixos-help').
 
-{ config, pkgs, ... }: rec {
+{ config, pkgs, inputs, ... }: rec {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   imports = [
     ./beefcake-hardware.nix
